d99e9773b56703a25b4afa6ca4e645bda5698b14
ishaanchip <ishaanchiplunkar@gmail.com>
2025-07-06 18:13:42 -0400

    big update after fbla

--- src/components/Course/Quiz/Quiz.jsx
+++ src/components/Course/Quiz/Quiz.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 import ".././Course.css"
 import "./Quiz.css"
 
@@ -9,13 +9,16 @@ import "./Quiz.css"
 import Header from '../../Header/Header.jsx'
 import { navigateFontSize } from '../courseHelper'
 import { fetchNvemAccount, fetchNvemCourse } from '../../generalHelper/simpleRoutes.js'
-import { starterText  } from './quizHelper.js'
+import { starterText, numberToLetter  } from './quizHelper.js'
 
 
 //external dependenices
 import { Link, useParams } from 'react-router-dom'
 import { useQuery } from '@tanstack/react-query'
 import { ArrowUpIcon, ArrowDownIcon, ChevronLeftIcon, ChevronRightIcon,  } from '@heroicons/react/24/solid'
+import { Button } from '@chakra-ui/react'
+import { toaster } from "../../ui/toaster"
+import { Alert, CloseButton } from '@chakra-ui/react'
 
 
 
@@ -32,15 +35,76 @@ const Quiz = () => {
             queryFn:async() => fetchNvemCourse(course_name, 'content'),
             staleTime:0
         })
-        const [currentSelections, setCurrentSelections] = useState(["", "", "", "", ""])
+  
         const [completeQuizData, setCompleteQuizData] = useState([]);
+        const [questionQuizData, setQuestionQuizData] = useState([]);
+        //getting dynamic scroll on questions
+        const questionViewRef = useRef([])
 
         useEffect(() =>{
             if (quizData){
                 console.log(quizData)
                 setCompleteQuizData(quizData.question_bank)
+                setQuestionQuizData( quizData.question_bank.map((question) => question.answer_choices))
+                questionViewRef.current = quizData.question_bank.map(
+                    (_, i) => questionViewRef.current[i] ?? React.createRef()
+                  );
             }
         }, [quizData])
+
+    //2. handling quiz question selection
+        const [currentSelectionString, setCurrentSelectionString] = useState(["", "", "", "", ""])
+        const [currentSelectionInteger, setCurrentSelectionInteger] = useState([-1, -1, -1, -1, -1])
+        const [validSubmission, setValidSubmission] = useState(false);
+        const [questionAttempted, setQuestionAttempted] = useState(false)
+        const handleQuestionSelection = (choice, index) =>{
+            setCurrentSelectionString(previousSelections => {
+                const tempSelection = [...previousSelections]
+                tempSelection[index] = choice;
+                return tempSelection
+            })
+            setCurrentSelectionInteger(previousSelections => {
+                const tempSelection = [...previousSelections]
+                tempSelection[index] = questionQuizData[index].indexOf(choice);
+                return tempSelection
+            })
+        }
+
+        const handleQuestionScroll = (currentIndex) =>{
+            const scrollSpecs = {
+                behavior: 'smooth',
+                block: 'start',
+                inline: 'nearest',
+            }
+
+            const nextItemIndex = (currentIndex + 1) % currentSelectionString.length;
+            if (questionViewRef.current[nextItemIndex]?.current){
+                questionViewRef.current[nextItemIndex].current.scrollIntoView(scrollSpecs)
+            }
+        }
+
+        const handleQuizFullyAnswered = () =>{
+            for (let i = 0; i < currentSelectionInteger.length; i++){
+                if (currentSelectionInteger[i] == -1){
+                    setValidSubmission(false)
+                    setQuestionAttempted(true)
+                    return false
+                }
+            }
+            setValidSubmission(true)
+            return true;
+        }
+
+        const handleQuizResults = () =>{
+            let score = 0;
+            for (let i =0; i < currentSelectionString.length; i++){
+                if (currentSelectionString[i] === completeQuizData[i].correct_choice){
+                    score++;
+                }
+            }
+            return score;
+        }
+
 
 
 
@@ -105,15 +169,27 @@ const Quiz = () => {
             </div>
         </div>
         <div className="quiz-area">
+            <div className="quiz-questions">
             {
                 completeQuizData.map((quizQuestion, i) =>(
 
-                    <div className="quiz-question">
+                    <div className="quiz-question" ref={questionViewRef.current[i]}>
                         <div className="quiz-content">
                             <h3 style={{fontSize:wordSize.h3}}>{i+1}. {quizQuestion?.question}</h3>
                             <div className="quiz-choices">
                                 {quizQuestion.answer_choices.map((choices) =>(
-                                    <div className="quiz-choice">
+                                    <div className="quiz-choice" 
+                                        style={
+                                        currentSelectionString[i] === (choices) 
+                                        ? {backgroundColor:'rgba(50, 90, 192, 0.7)', color:'whitesmoke'} 
+                                        : {}}
+                                        onClick={() => {
+                                            handleQuestionSelection(choices, i)
+                                            if (i !== currentSelectionString.length - 1)
+                                                handleQuestionScroll(i)
+                                            }}
+                                        
+                                    >
                                         <p style={{fontSize:wordSize.p}}>{choices}</p>
                                     </div>
                                 ))}
@@ -129,7 +205,46 @@ const Quiz = () => {
 
                 ))
             }
+            </div>
+            
+            <div className="quiz-status">
+                <div className='question-state-area'>
+                    {
+                        currentSelectionInteger.map((selection, i) => (
+                            <p className="question-state" onClick={() => handleQuestionScroll(i - 1)}>
+                                Question #{i + 1}: {numberToLetter[selection]}
+                            </p>
+                        ))
+                    }
+                </div>
+                <Button 
+                    w="35%" m="2%" variant="outline" size="sm"className='button' 
+                    onClick={() => {
+                        if (handleQuizFullyAnswered() === true){
+                            console.log('complete!')
+                        }
+                    }}
+                >
+                    Submit Quiz
+                </Button>
+                
+
+            </div>
+
         </div>
+        {
+        (validSubmission === false && questionAttempted === true) &&
+            <Alert.Root status="error" className='alert-tag'>
+                <Alert.Indicator />
+                <Alert.Content>
+                    <Alert.Title>Incomplete Submission</Alert.Title>
+                    <Alert.Description>
+                    Answer all questions to submit!
+                    </Alert.Description>
+                </Alert.Content>
+                <CloseButton pos="relative" top="0" insetEnd="0" onClick={() => setValidSubmission(true)}/>
+            </Alert.Root>
+        }
 
         <div className="navigation-area">
             <Link className="prev-nav" to={`/code-task/${course_name}`}>
--- src/components/Course/Quiz/quizHelper.js
+++ src/components/Course/Quiz/quizHelper.js
@@ -2,3 +2,11 @@
     export const starterText = {
         mini_description: "it's time to put your learning to the test!"
     }
+
+    export const numberToLetter = {
+        '-1':"-",
+        '0':"A",
+        '1':"B",
+        '2':"C",
+        '3':"D"
+    }
